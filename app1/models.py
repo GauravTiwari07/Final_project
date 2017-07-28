1442abbc51bf506ed1f33317d300ea7a647cba29
GauravTiwari07 <tgaurav72@gmail.com>
2017-07-28 22:54:44 +0530

    added vote the comments

--- app1/models.py
+++ app1/models.py
@@ -55,3 +55,12 @@ class CommentModel(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     updated_on = models.DateTimeField(auto_now=True)
 
+
+@property
+def upvote(self):
+    return len(LikeComm.objects.filter(comment=self))
+
+
+class LikeComm(models.Model):
+    user = models.ForeignKey(UserModel)
+    comment = models.ForeignKey(CommentModel)
